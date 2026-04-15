be57b0462c2542b0872b31c54fab1200b857fa3f
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-04-15 19:43:26 -0300

    Ajustado AppDatabase

--- app/src/main/java/com/example/provatofoli/AppDatabase.java
+++ app/src/main/java/com/example/provatofoli/AppDatabase.java
@@ -7,14 +7,18 @@ import androidx.room.RoomDatabase;
 
 @Database(entities = {Product.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+
     private static AppDatabase instance;
+
     public abstract ProductDao productDao();
 
     public static synchronized AppDatabase getInstance(Context context) {
         if (instance == null) {
-            instance = Room.databaseBuilder(context.getApplicationContext(),
-                            AppDatabase.class, "product_database")
-                    .fallbackToDestructiveMigration()
+            instance = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            AppDatabase.class,
+                            "product-database"
+                    ).fallbackToDestructiveMigration()
                     .allowMainThreadQueries()
                     .build();
         }

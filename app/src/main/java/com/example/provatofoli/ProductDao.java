d5cfe72683ab81ca771e00e19fc7b4f479e4f685
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-04-15 19:44:44 -0300

    Ajustado ProductDAO, com a inserção dos métodos para operar o banco de dados

--- app/src/main/java/com/example/provatofoli/ProductDao.java
+++ app/src/main/java/com/example/provatofoli/ProductDao.java
@@ -5,11 +5,18 @@ import androidx.room.Insert;
 import androidx.room.Query;
 import java.util.List;
 
+// O Data Access Object é o componente que serve para persistência de dados.
+// Serve para definir as operações que podem ser feitas no banco de dados com relação à entidade Product
 @Dao
 public interface ProductDao {
+
+    // Método para inserir um produto na tabela do BD
+    // A anotação @Insert informa à Room que este método deve ser usado para inserir dado
     @Insert
     void insert(Product product);
 
+    // Método para buscar todos os produtos cadastrados no BD
+    // A anotação @Query permite definir uma consulta SQL personalizada
     @Query("SELECT * FROM products")
     List<Product> getAllProducts();
 }
